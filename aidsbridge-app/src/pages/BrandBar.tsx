13488d27cb8e5e203134cb1ecb45a561a4dfb2f0
Yinhao Zhao <zhao.yinh@northeastern.edu>
2024-04-15 23:42:24 -0400

    ui init

---
+++ aidsbridge-app/src/pages/BrandBar.tsx
@@ -0,0 +1,31 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+// BrandBar MUI learned from last week's lecture
+function BrandBar() {
+    return (
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Aids Bridge
+          </Typography>
+          <Button color="inherit"></Button>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default BrandBar
