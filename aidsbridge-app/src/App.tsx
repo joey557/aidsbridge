e2c7d1b9ff73822615428e85fb9b51119d55cbcc
Zuyi Huang <huang.zu@northeastern.edu>
2024-04-16 14:36:48 -0400

    add background image and style

--- aidsbridge-app/src/App.tsx
+++ aidsbridge-app/src/App.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import BrandBar from './pages/BrandBar';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
       <CssBaseline />
       <BrandBar />
-      
+      <HomePage />
     </>
   );
 }
---
+++ aidsbridge-app/src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import smileImage from  '../assets/smile-people.jpg';
+
+const HomePage: React.FC = () => {
+    const backgroundStyle = {
+        backgroundImage: `url(${smileImage})`,
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center 15%',
+        backgroundRepeat:'no-repeat',
+        height: '70vh',  
+        width: '100vw', 
+        display: 'flex',
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        color: 'white',
+        fontSize: '2rem', 
+
+    };
+    return(
+        <div style={backgroundStyle}>
+            <h1>To protect everyone’s health, <br /> protect everyone’s rights </h1>
+        </div>
+    );
+};
+export default HomePage;
