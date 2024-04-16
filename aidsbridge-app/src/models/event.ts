7f3fa336d056f3a4e5e894ed1c2cd1160dc705dd
Yinhao Zhao <zhao.yinh@northeastern.edu>
2024-04-16 00:00:36 -0400

    upload ts models

---
+++ aidsbridge-app/src/models/event.ts
@@ -0,0 +1,14 @@
+export interface EventPerson {
+    peopleName: string;
+    accountId: string;
+  }
+  
+  export interface Event {
+    _id: string;
+    title: string;
+    content: string;
+    creator: string;
+    people: EventPerson[];
+    createdDate: Date;
+    eventsDate: Date;
+  }
