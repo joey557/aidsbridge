6392eea6db774f31274cc4c1c849c55b2184eae0
Zuyi Huang <huang.zu@northeastern.edu>
2024-04-17 01:06:28 -0400

    add react-countup

---
+++ aidsbridge-app/src/components/react-countup.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CountUp from 'react-countup';
+import { Typography, Box } from '@mui/material';
+
+interface StatisticProps {
+    
+    endValue: number;
+    decimals: number;
+    duration: number;
+    label: string;
+    description: string;
+}
+
+const Statistic: React.FC<StatisticProps> = ({endValue,decimals, duration, label, description}) =>{
+    return (
+        <Box textAlign= "center" >
+            <Typography variant='h4' sx={{ fontWeight: 'bold', color: "#cf233a" }} gutterBottom>
+                <CountUp 
+                end={endValue} 
+                decimals={decimals}
+                duration={duration} 
+                enableScrollSpy
+                scrollSpyDelay={100} // 100 milliseconds delay
+                /><br/>
+                {label}
+            </Typography>
+            <hr style={{ width: '300px', border: '1px solid #cf233a', marginBottom: '20px' }} />
+            <Typography variant='subtitle2' gutterBottom style={{ color: '#3c3c3c' }}>
+                {description}
+            </Typography>
+            
+        </Box>
+    );
+};
+
+export default Statistic;
--- aidsbridge-app/src/pages/HomePage.tsx
+++ aidsbridge-app/src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import smileImage from  '../assets/smile-people.jpg';
 import HomePageArticle from '../components/homepage-article';
-
+import Statistic from '../components/react-countup';
+import { Box } from '@mui/material';
 
 
 
@@ -18,6 +19,7 @@ const HomePage: React.FC = () => {
         alignItems: 'center', 
         color: 'white',
         fontSize: '2rem', 
+        marginBottom: '100px',
 
     };
     return(
@@ -25,6 +27,16 @@ const HomePage: React.FC = () => {
             <div style={backgroundStyle}>
                 <h1>To protect everyone’s health, <br /> protect everyone’s rights </h1>
             </div>
+            <Box textAlign = "center">
+                <h2>AIDS BY THE NUMBERS</h2>
+                <p>See the latest data on HIV.</p>
+            </Box>
+            <Box maxWidth="80%" margin="0 auto" padding="20px" display={'flex'} flexDirection='row' justifyContent="space-between" alignItems="center">
+                <Statistic endValue={1.3} decimals={1} duration={2.75} label="MILLION" description="people were newly infected with HIV in 2022" />
+                <Statistic endValue={39} decimals={0} duration={2.75} label="MILLION" description="people were living with HIV in 2022" />
+                <Statistic endValue={630} decimals={0} duration={2.75} label="THOUSAND" description="people died of AIDS-related illnesses in 2022" />
+            </Box>
+
             <HomePageArticle />
         </>
     );
