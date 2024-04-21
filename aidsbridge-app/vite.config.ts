01d480a5b1115f0f37631232b1b9ed28b4d56510
Zuyi Huang <huang.zu@northeastern.edu>
2024-04-21 19:29:18 -0400

    pwa

--- aidsbridge-app/vite.config.ts
+++ aidsbridge-app/vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          "name": "CourseRegistration",
+          "short_name": "CourseRegist",
+          "start_url": "./",
+          "display": "standalone",
+          "background_color": "#fff",
+          "description": "Course Registration App.",
+          "theme_color": "#ffffff",
+          "icons": [
+            {
+              "src": "src/images/pwa-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "src/images/pwa.png",
+              "sizes": "144x144",
+              "type": "image/png",
+              "purpose": "any"
+            }
+          ]
+        }
+      }
+    )
+  ]
 })
