936865834013a879c014a4697d85aee692b881ed
Yinhao Zhao <zhao.yinh@northeastern.edu>
2024-04-22 18:58:39 -0400

    change app

--- aidsbridge-service/app/controllers/aidsbridge-controller.js
+++ aidsbridge-service/app/controllers/aidsbridge-controller.js
@@ -86,26 +86,20 @@ export const deleteEvent = async (req, res) => {
 }
 
 export const updateEvent = async (req, res) => {
-    const eventId = req.params.id; 
-    const person = req.body.person;
-
     try {
         const updatedEvent = await Event.findByIdAndUpdate(
             eventId,
             { $push: { people: person } },  
             { new: true, safe: true, upsert: false }
-        );
-
-        if (updatedEvent) {
-            res.status(200).json(updatedEvent);
-        } else {
-            throw new Error('Event not found or update failed');
-        }
+          );
+        const id = req.params.id;
+        const content = { ...req.body };
+        //const updatedEvent = await eventService.updateEvents(id, content);
+        setResponse(updatedEvent, res);
     } catch (error) {
-        res.status(500).json({ message: 'Failed to update event', error: error.toString() });
+        setError(error, res);
     }
 }
-
 
 export const filterEvents = async (req, res) => {
     try {
