46f5a0f44c154ce487aa53073fd03e4ad5319575
Zuyi Huang <huang.zu@northeastern.edu>
2024-08-29 15:40:44 -0400

    add file upload functionality with image type validation

--- aidsbridge-service/app/middleware/multerConfig.js
+++ aidsbridge-service/app/middleware/multerConfig.js
@@ -1,14 +1,23 @@
-import multer from 'multer';
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
-  }
+    cb(null, file.originalname);
+  },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb(new Error("Unsupported file type"), false);
+    }
+  },
+});
 
 export default upload;
